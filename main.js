a25ef000ed11c66b9f522595bf42164d6e73bf29
Praisejnr <praiseomoniyi65@gmail.com>
2025-03-15 16:01:49 +0100

    Initial commit for learninghub

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+ 
+const hamburger = document.getElementById('hamburger');
+const navBar =  document.getElementById('nav-bar');
+
+hamburger.classList.toggle('active');
+navBar.classList.toggle('active');
+};
